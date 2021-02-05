243f713f8c44e0e2c420e6ffe5f7f00fb4d4560f
DS <dextershankle@gmail.com>
2021-02-05 14:44:06 -0700

    init commit
    Former-commit-id: 4bf95e7ca135e45a746638183a06b46e5dbf1425

---
+++ Z2/main.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Z2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+           
+            var b = new Comms();
+            //thread 1 runs command
+            //thread 2 checks in / retrieves and delivers commands and results
+            b.checkin();
+          
+            
+        }
+    }
+}
